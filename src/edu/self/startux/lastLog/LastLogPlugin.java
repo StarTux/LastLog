8976b54d74c047e9708df0cabbf8f8706a35bafb
Thomas Loy <ths.loy@googlemail.com>
2012-04-27 15:24:49 +0200

    adapt to new changes

--- src/edu/self/startux/lastLog/LastLogPlugin.java
+++ src/edu/self/startux/lastLog/LastLogPlugin.java
@@ -1,4 +1,4 @@
-/*
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Copyright 2012 StarTux.
  *
  * This file is part of LastLog.
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with LastLog.  If not, see <http://www.gnu.org/licenses/>.
- */
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 package edu.self.startux.lastLog;
 
@@ -35,8 +35,8 @@ public class LastLogPlugin extends JavaPlugin implements Listener {
         final public static String NOTIFY_PERMISSION = "lastlog.notify";
         private PlayerList firstlogList;
         private PlayerList lastlogList;
-        private FirstLogExecutor firstLogExecutor = new FirstLogExecutor(this);
-        private LastLogExecutor lastLogExecutor = new LastLogExecutor(this);
+        private LastLogExecutor firstLogExecutor = new LastLogExecutor(this, false);
+        private LastLogExecutor lastLogExecutor = new LastLogExecutor(this, true);
         private LogInfoExecutor logInfoExecutor = new LogInfoExecutor(this);
 
         // // debug function. requires command registration!
@@ -55,7 +55,7 @@ public class LastLogPlugin extends JavaPlugin implements Listener {
                 getServer().getPluginManager().registerEvents(this, this);
                 logger = getServer().getLogger();
                 // Apparently what takes the most time are the following I/O heavy instructions.
-                // Hence, their output will be cached.
+                // Hence, their output will be cached. Initialize the cache with bukkit data.
                 OfflinePlayer[] players = getServer().getOfflinePlayers();
                 firstlogList = new PlayerList(players, false);
                 lastlogList = new PlayerList(players, true);
@@ -68,18 +68,19 @@ public class LastLogPlugin extends JavaPlugin implements Listener {
                 lastlogList = null;
         }
 
-        // when a player joins, the caches have to be updated
+        /**
+         * Update the appropriate caches when a player joins.
+         * At this point we get the time from System rather than
+         * from Bukkit as the latter turned out faulty at times.
+         */
         @EventHandler
         public void onPlayerJoin(PlayerJoinEvent event) {
                 Player player = event.getPlayer();
                 String name = player.getName();
-                long first = player.getFirstPlayed();
-                long last = player.getLastPlayed();
-                // on first login, getLastPlayed() returns 0.
-                // 1970-01-01 is over, so let's not use it.
-                if (last == 0l) last = first;
+                long last = System.currentTimeMillis();
                 lastlogList.set(name, last);
                 if (!player.hasPlayedBefore()) {
+                        long first = last;
                         firstlogList.set(name, first);
                         String message = LastLogColors.UNKNOWN + name + LastLogColors.HEADER + " has logged in for the first time";
                         // getServer().broadcast(message, NOTIFY_PERMISSION); // this acts weird
@@ -90,26 +91,6 @@ public class LastLogPlugin extends JavaPlugin implements Listener {
                                 }
                         }
                 }
-        }
-
-        // handle common tasks of firstlog and lastlog
-        public boolean executeSomeLog(CommandSender sender, String label, String[] args, boolean lastlog) {
-                if (args.length > 1) {
-                        return false;
-                }
-                int pageNumber = 0;
-                if (args.length == 1) {
-                        try {
-                                pageNumber = Integer.parseInt(args[0]) - 1;
-                        } catch (IllegalArgumentException iae) {
-                                return false;
-                        }
-                }
-                if (pageNumber < 0) return false;
-                PlayerList playerList = getPlayerList(lastlog);
-                playerList.sort();
-                playerList.displayPage(pageNumber, lastlog, sender);
-                return true;
         }
 
         public PlayerList getPlayerList(boolean lastlog) {
